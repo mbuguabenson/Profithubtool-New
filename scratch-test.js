d8c8a1bb23f87846c8c90d018e5a49b22c152c28
benso@Benadesigns <mbuguabenson2020@gmail.com>
2026-05-03 11:01:59 +0300

    chore: replace project with official trading-bot-template

---
+++ scratch-test.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://api.derivws.com/trading/v1/options/ws/public?app_id=121856');
+
+ws.on('open', function open() {
+  ws.send(JSON.stringify({ active_symbols: 'brief', req_id: 1 }));
+});
+
+ws.on('message', function incoming(data) {
+  console.log(data.toString());
+  ws.close();
+});
+
+ws.on('error', function error(err) {
+  console.error(err);
+});
